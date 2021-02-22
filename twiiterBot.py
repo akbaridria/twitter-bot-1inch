70afc26ba1c3d94efb8621ad5b1fbe02f651bc3b
unknown <akbar@LAPTOP-QDL8NMD7.Supporting.corp.bankmandiri.co.id>
2021-02-23 02:56:49 +0700

    add try catch error

--- twiiterBot.py
+++ twiiterBot.py
@@ -60,32 +60,35 @@ def tweet(api, status) :
 
 prevStatus = ""
 while True :
-  r = requests.post(request_url, json = {'query' : query})
-  raw_data = r.json()
-  if r.status_code == 200 :
-    data = raw_data['data']['swaps'][0]
-    ticker0Symbol = data['pair']['token0']['symbol']
-    ticker1Symbol = data['pair']['token1']['symbol']
-    amountOut = data['amount0Out']
-    decimal0Token = data['pair']['token0']['decimals']
-    decimal2Token = data['pair']['token1']['decimals']
-    parameter1inch = {
-        "fromTokenAddress" : data['pair']['token0']['id'],
-        "toTokenAddress" : data['pair']['token1']['id'],
-        "amount" : int(float(math.pow(10, int(decimal0Token))) * float(amountOut))
-    }
-    r3 = requests.get(url_1inch, params=parameter1inch)
-    uniswapValue = data['amount1In']
-    print('bot is running')
-    inch1Value = float(r3.json()['toTokenAmount'])/math.pow(10, int(decimal2Token))
-    if inch1Value > float(uniswapValue) :
-        tx = data['transaction']['id']
-        timestamp = datetime.datetime.fromtimestamp(int(data['transaction']['timestamp']))
-        waktu = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        status = "The swap transaction were carried out at Uniswap on {} from token {} {} for token {} {} tx : {}, \n \n if using 1inch protocol you will get {} {} for {} {}.".format(waktu, amountOut, ticker0Symbol, uniswapValue, ticker1Symbol, tx,  inch1Value, ticker1Symbol, amountOut, ticker0Symbol)
-        if prevStatus != status :
-            api = connect_to_twitter()
-            tweet(api, status)
-            prevStatus = status
+  try :
+    r = requests.post(request_url, json = {'query' : query})
+    raw_data = r.json()
+    if r.status_code == 200 :
+        data = raw_data['data']['swaps'][0]
+        ticker0Symbol = data['pair']['token0']['symbol']
+        ticker1Symbol = data['pair']['token1']['symbol']
+        amountOut = data['amount0Out']
+        decimal0Token = data['pair']['token0']['decimals']
+        decimal2Token = data['pair']['token1']['decimals']
+        parameter1inch = {
+            "fromTokenAddress" : data['pair']['token0']['id'],
+            "toTokenAddress" : data['pair']['token1']['id'],
+            "amount" : int(float(math.pow(10, int(decimal0Token))) * float(amountOut))
+        }
+        r3 = requests.get(url_1inch, params=parameter1inch)
+        uniswapValue = data['amount1In']
+        print('bot is running')
+        inch1Value = float(r3.json()['toTokenAmount'])/math.pow(10, int(decimal2Token))
+        if inch1Value > float(uniswapValue) :
+            tx = data['transaction']['id']
+            timestamp = datetime.datetime.fromtimestamp(int(data['transaction']['timestamp']))
+            waktu = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            status = "The swap transaction were carried out at Uniswap on {} from token {} {} for token {} {} tx : {}, \n \n if using 1inch protocol you will get {} {} for {} {}.".format(waktu, amountOut, ticker0Symbol, uniswapValue, ticker1Symbol, tx,  inch1Value, ticker1Symbol, amountOut, ticker0Symbol)
+            if prevStatus != status :
+                api = connect_to_twitter()
+                tweet(api, status)
+                prevStatus = status
+  except :
+      print("there's an error happen!")
 
     
